7cee57b35eef7164ff77e9e395ef72f33aef8d2e
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2024-01-03 11:02:28 +0200

    Started working on animated navbar

--- app/src/main/java/me/gm/twaddle/HomeActivity.java
+++ app/src/main/java/me/gm/twaddle/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,6 +12,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
